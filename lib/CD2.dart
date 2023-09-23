8c1c54acee732d78ae5e4dcc1e613be843298f83
elyon <elyonccari7692@gmail.com>
2023-09-22 20:27:26 -0500

    C2

--- lib/CD2.dart
+++ lib/CD2.dart
@@ -24,29 +24,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   bool isTitleBold = true;
-  double peso = 80;
-  double altura = 100;
+  double peso = 70; 
+  double altura = 170;
   String resultado = '';
-  String categoria = '';
-  String mensaje = '';
-  int numero = 0;
+
   void calcular() {
+    
     double imc = peso / ((altura / 100) * (altura / 100));
 
     setState(() {
-      numero = imc.toInt();
       if (imc < 18.5) {
-        resultado = 'PESO BAJO ';
-        categoria = 'pesobajo';
-        mensaje = 'Debes  comer un poco más.';
+        resultado = 'BAJO PESO';
       } else if (imc < 24.9) {
         resultado = 'NORMAL';
-        categoria = 'normal';
-        mensaje = 'Estas muy bien,sigue asi realiza actividad fisica.';
       } else {
         resultado = 'SOBREPESO';
-        categoria = 'sobrepeso';
-        mensaje = 'alimentate sanamente y realiza mas actividad fisica.';
       }
     });
   }
@@ -62,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               "Bienvenido,selecciona tu peso y altura",
-              style: TextStyle(
+               style: TextStyle(
                 fontSize: 20.0,
                 fontWeight:
                     isTitleBold == true ? FontWeight.bold : FontWeight.normal,
@@ -75,80 +67,40 @@ class _MyHomePageState extends State<MyHomePage> {
             Slider(
               value: peso,
               min: 10,
-              max: 300,
+              max: 200,
               onChanged: (newValue) {
                 setState(() {
                   peso = newValue;
                 });
               },
               activeColor: Colors.pink,
-              inactiveColor: Colors.pink,
+              inactiveColor: Colors.pink.withOpacity(0.2),
             ),
             Text(
-              '${altura.toStringAsFixed(2)}cm',
-              style: TextStyle(fontSize: 20),
+              ' ${altura.toStringAsFixed(1)}cm',
+              style: TextStyle(fontSize: 18),
             ),
             Slider(
               value: altura,
               min: 100,
-              max: 300,
+              max: 220,
               onChanged: (newValue) {
                 setState(() {
                   altura = newValue;
                 });
               },
               activeColor: Colors.pink,
-              inactiveColor: Colors.pink,
+              inactiveColor: Colors.pink.withOpacity(0.2),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: calcular,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/imagenes/play.png',
-                      width: 30, height: 30), 
-                  SizedBox(width: 10), 
-                  Text('Calcular'), 
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 20),
-                  Text(
-                    'Resultado: ',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            if (categoria.isNotEmpty)
-              Image.asset(
-                'assets/imagenes/$categoria.png',
-                width: 230,
-                height: 230,
-              ),
-               SizedBox(height: 20),
-               if (numero> 0)
-            Text(
-              '$numero',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,color:Colors.pink ),
+              child: Image.asset('assets/imagenes/play.png'), 
             ),
             SizedBox(height: 20),
             Text(
-              '$resultado',
+              'Resultado: $resultado',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              '$mensaje',
-              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
